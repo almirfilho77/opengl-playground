9d7d76cf38970b74b6721b69bb5db81b634e2f2b
almirfilho77 <almir.filho77@gmail.com>
2024-04-07 22:59:56 -0300

    Add minor changes to the code
        - Set the viewport
        - Set the clear color
        - Set GL version and profile (Core)

--- opengl-playground/src/Application.cpp
+++ opengl-playground/src/Application.cpp
@@ -25,14 +25,14 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);                    // Need to specify in order for the other hints to work
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);                    // Need to specify in order for the other hints to work
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                    // Need to specify in order for the other hints to work
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                    // Need to specify in order for the other hints to work
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);  // It implicitly creates and binds a VAO
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);    // Need to explicitly create and bind VAO
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);    // Need to explicitly create and bind VAO
 
     /* Create a windowed mode window and its OpenGL context */
-    int width = 960;
-    int height = 540;
+    int width = 800;
+    int height = 600;
     window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -45,8 +45,6 @@ int main(void)
     glfwMakeContextCurrent(window);
     std::cout << "GL VERSION = " << glGetString(GL_VERSION) << "\n";
 
-    glfwSwapInterval(1);
-
     GLenum error = glewInit();
     if (error != GLEW_OK)
     {
@@ -54,6 +52,13 @@ int main(void)
         return -1;
     }
     std::cout << "GLEW VERSION = " << glewGetString(GLEW_VERSION) << "\n";
+
+    // Specify the render area of the window
+    GLCallVoid(glViewport(0, 0, width, height));
+
+    // Base color of the window
+    GLCallVoid(glClearColor(0.07f, 0.13f, 0.17f, 1.0f));
+    GLCallVoid(glClear(GL_COLOR_BUFFER_BIT));
 
     // Enable alpha channel rendering
     GLCallVoid(glEnable(GL_BLEND));
@@ -113,6 +118,8 @@ int main(void)
 
         Renderer renderer;
 
+        glfwSwapInterval(1);
+        
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
@@ -143,6 +150,10 @@ int main(void)
         }
     }
 
+    // Destroy the window
+    glfwDestroyWindow(window);
+    
+    // Terminate GLFW
     glfwTerminate();
     return 0;
 }
